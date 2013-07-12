6fa46a453985a3b1f1116dfe3243d7453fe0d7d0
Raju Maharjan <E1514779@Rajus-MacBook-Pro.local>
2013-07-12 12:52:42 -0500

    Initial Commit

---
+++ MHDR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MHDR
+//
+//  Created by Raju Maharjan on 7/12/13.
+//  Copyright (c) 2013 Memorial Hermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mhAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mhAppDelegate class]));
+    }
+}
---
+++ MHDR/mhAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  mhAppDelegate.h
+//  MHDR
+//
+//  Created by Raju Maharjan on 7/12/13.
+//  Copyright (c) 2013 Memorial Hermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mhAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
